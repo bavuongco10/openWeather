50343bb59472a56c74bcce449e404dc34dfe5a92
bavuongco10 <bavuongco10@gmail.com>
2016-11-13 02:35:09 +0700

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var express = require('express');
+let express = require('express');
 
 // Create our app
-var app = express();
+let app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect(`http:// ${req.hostname + req.url}`);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Express server is up on port 3000');
 });
